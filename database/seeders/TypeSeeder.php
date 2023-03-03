5525e7b700b9012febed1ca20ae4ac063ae67b86
Frah01 <vinario.francesco@gmail.com>
2023-03-03 13:51:04 +0100

    Fatta nuove migration con foreign key

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
