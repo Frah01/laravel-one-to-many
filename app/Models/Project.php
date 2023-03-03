6e7ab2d8161e019e8f9ec81835ac0576fb791dc8
Frah01 <vinario.francesco@gmail.com>
2023-03-03 13:58:51 +0100

    Models modificati

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,11 +8,15 @@ use Illuminate\Support\Str;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'content', 'slug', 'type_id'];
     use HasFactory;
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
 
 class Type extends Model
 {
+    protected $fillable = ['name', 'slug'];
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
     use HasFactory;
 }
